53c0adc3f37859373c73b24f73ba4f8d6c80850f
Younes Darabi <younes.darabi@outlook.com>
2026-02-22 22:36:30 +0100

    GET,DELETE,PUT

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import  path
-from .views import markets_view
+from .views import markets_view, markets_single_view
 
 urlpatterns = [
     path('',markets_view),
+    path('<int:pk>/',markets_single_view),
 ]
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -20,3 +20,26 @@ def markets_view(request):
         else:
             return Response(serializer.errors)
 
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def markets_single_view(request, pk):
+
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)
