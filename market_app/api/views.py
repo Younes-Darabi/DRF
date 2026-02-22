f2e9bc9f126896169f267ee966f664afd18f4352
Younes Darabi <younes.darabi@outlook.com>
2026-02-22 22:19:41 +0100

    Serializer

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,14 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from .serializers import MarketSerializer
+from market_app.models import Market
 
-@api_view(['GET','POST'])
-def first_view(request):
+
+@api_view(['GET', 'POST'])
+def markets_view(request):
+
     if request.method == 'GET':
-        return Response({"message": "Hello, world!"})
+        markets = Market.objects.all()
+        serializer = MarketSerializer(markets, many=True)
+        return Response(serializer.data)
+
     if request.method == 'POST':
-        try:
-            msg = request.data['message']
-            return Response({"Your message": msg}, status=status.HTTP_201_CREATED)
-        except:
-            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = MarketSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
